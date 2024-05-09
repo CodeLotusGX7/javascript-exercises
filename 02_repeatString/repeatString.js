4f3d93f702aee9181fc8b71f9443651d6d6a9402
CodeLotusGX7 <114036124+CodeLotusGX7@users.noreply.github.com>
2024-05-08 23:35:19 -0400

    repeatString test 1 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
+//let inputString = "hey";
+//let repeatNumber = 3;
 
+const repeatString = function(inputString,repeatNum) {
+    let newString = '';
+    for (let i = 0; i < repeatNum; i++) {
+        newString += inputString;
+    }
+    return newString;
+
+    /*
+    for (let currentNum = 0; repeatNumber > currentNum; currentNum++) {
+        console.log("current num = " + currentNum);
+        console.log("repeat num = " + repeatNumber);
+
+        return inputString;
+        
+    }
+    */
 };
+// repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
