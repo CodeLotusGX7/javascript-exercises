6150b31e99222a206579c217f29bea5c0a056684
CodeLotusGX7 <114036124+CodeLotusGX7@users.noreply.github.com>
2024-05-12 22:23:18 -0400

    05 sumAll javascript exercise test 1-4 passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secNum) {
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secNum)) return "ERROR";
+    if (firstNum < 0 || secNum < 0) return "ERROR";
+
+    if (firstNum > secNum) {
+        topNum = firstNum;
+        botNum = secNum;
+        console.log("topNum = " + topNum);
+        console.log("botNum = " + botNum);
+   
+    }
+    if (firstNum < secNum) {
+        topNum = secNum;
+        botNum = firstNum;
+        console.log("topNum = " + topNum);
+        console.log("botNum = " + botNum);
+    }
+
+    let sum = 0;
+    for (let i = firstNum; i <= secNum; i++) {
+        sum += i;
+    }
+    return sum;
+
 
 };
 
