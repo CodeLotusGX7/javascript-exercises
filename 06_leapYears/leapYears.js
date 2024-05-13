41e8c5006828b7e236c3b5f3f1f8bbe51a660090
CodeLotusGX7 <114036124+CodeLotusGX7@users.noreply.github.com>
2024-05-13 11:27:09 -0400

    06 leap year javascript exercise complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,30 @@
-const leapYears = function() {
+const leapYears = function(testYear) {
+    
+    if (testYear / 400 &&  (testYear % 400) == 0) {
+        console.log("divisible by 400");
+        console.log("is a leap year");
+        return true;
+        
+    } else if (testYear / 100 &&  (testYear % 100) == 0) {
+        console.log("divisible by 100");
+        console.log("is not a leap year");
+        return false;
+        
+        
+    } else if  (testYear / 4 &&  (testYear % 4) == 0) {
+        console.log("divisible by 4");
+        console.log("is a leap year");
+        return true;
+        
+    } else {
+        console.log("not divisible by 4");
+        console.log("is not a leap year");
+        return false;
+
+
+    }
+
+    
 
 };
 
